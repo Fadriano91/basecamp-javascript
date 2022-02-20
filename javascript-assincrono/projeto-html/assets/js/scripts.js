55cfa9237b3ef9794ce4c04b61ebbefa2d31f8cf
Fadriano91 <fquimico@hotmail.com.br>
2022-02-20 19:12:39 -0300

    adicionando api

--- javascript-assincrono/projeto-html/assets/js/scripts.js
+++ javascript-assincrono/projeto-html/assets/js/scripts.js
@@ -1,21 +1,19 @@
 const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBtn = document.getElementById('change-cat');
+const catImg = document.getElementById('cat');
 
 const getCats = async () => {
-	try {
-		const data = await fetch(BASE_URL);
-		const json = await data.json();
-		return json.webpurl;
-	} catch (e) {
-		console.log(e.message);
-	}
-};
+		const data = await fetch(BASE_URL)
+		.then(res=> res.json())
+		.cath( e => console.log(e))
+
+		return data.webpurl;
+	};
 
 const loadImg = async () => {
-	const img = document.getElementsByTagName('img')[0];
-	img.src = await getCats();
+	catImg.src = await getCats(); 
 };
 
-loadImg();
+catBtn.addEventListener('click', loadImg);
 
-const btn = document.getElementById('change-cat');
-btn.addEventListener('click', loadImg);
+loadImg();
